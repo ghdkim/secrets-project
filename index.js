26680cc5a19c907591a4da3af932214d09eda756
ghdkim <146387183+ghdkim@users.noreply.github.com>
2024-07-30 17:11:18 +0800

    summary

---
+++ index.js
@@ -0,0 +1,26 @@
+import express from "express";
+import axios from "axios";
+
+const app = express();
+const port = 3000;
+
+const URL = "https://secrets-api.appbrewery.com/random";
+
+app.use(express.static("public"));
+
+app.get("/", async (req, res) => {
+  try {
+    const result = await axios.get(URL);
+    res.render("index.ejs", {
+      secret: result.data.secret,
+      user: result.data.username,
+    });
+  } catch (error) {
+    console.log(error.response.data);
+    res.status(500);
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
